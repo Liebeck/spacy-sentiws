80dc46ac227e6b53df53d7d9fb6cafba14bb8171
Matthias Liebeck <Liebeck@users.noreply.github.com>
2018-04-24 18:52:19 +0200

    Fix Travis download script

--- .travis/download_sentiws.sh
+++ .travis/download_sentiws.sh
@@ -1,6 +1,7 @@
 export DOWNLOADURL='http://pcai056.informatik.uni-leipzig.de/downloads/etc/SentiWS/SentiWS_v1.8c.zip'
 
 wget -O sentiws.ZIP $DOWNLOADURL
+mkdir -p data
 unzip sentiws.ZIP -d data/sentiws/
 rm data/sentiws/SentiWS.txt
 rm -r data/sentiws/__MACOSX
